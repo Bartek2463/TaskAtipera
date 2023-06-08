c546a6d2d4a1c56303bbbcf8019b9cd71a6d4265
Bartek2463 <szybiakbartlomiej@gmail.com>
2023-06-09 01:32:30 +0200

    Add package exception and add class ErrorDecoder Error handling

---
+++ src/main/java/com/example/taskatipera/controller/ControllerGitHub.java
@@ -0,0 +1,26 @@
+package com.example.taskatipera.controller;
+
+import com.example.taskatipera.model.RepositoryGitHub;
+import com.example.taskatipera.service.ServiceGithub;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/repos")
+public class ControllerGitHub {
+    private final ServiceGithub serviceGithub;
+
+    public ControllerGitHub(ServiceGithub serviceGithub) {
+        this.serviceGithub = serviceGithub;
+    }
+
+    @GetMapping("/{userName}")
+    public Set<RepositoryGitHub> findAllRepository(@PathVariable("userName")String userName){
+        return serviceGithub.findOther(userName)
+                .orElseThrow(()->new RuntimeException());
+    }
+}
