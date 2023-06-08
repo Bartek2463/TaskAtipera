9b8dbfceef02127f3115de3dd8cac910c1ee72a8
Bartek2463 <szybiakbartlomiej@gmail.com>
2023-06-08 23:39:18 +0200

    Create Interface for connect to Api by Feing Client (declarative client)

---
+++ src/main/java/com/example/taskatipera/service/ClientGitHub.java
@@ -0,0 +1,23 @@
+package com.example.taskatipera.service;
+
+import com.example.taskatipera.dto.GitRepositoryDTO;
+import com.example.taskatipera.model.Branch;
+import feign.Headers;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
+@FeignClient(name = "git-api",url = "https://api.github.com")
+@Headers("Accept: application/json")
+public interface ClientGitHub {
+
+    @GetMapping("/users/{userName}/repos")
+    List<GitRepositoryDTO> getRepoGit(@PathVariable("userName")String userName);
+
+    @GetMapping("/repos/{userName}/{repoName}/branches")
+    List<Branch> getBranch(@PathVariable("userName")String userName,@PathVariable("repositoryName")String repositoryName);
+}
