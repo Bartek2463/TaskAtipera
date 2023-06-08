ad276c4d4bdd3fb689a1e8c8dcd042afd9ae974b
Bartek2463 <szybiakbartlomiej@gmail.com>
2023-06-08 23:28:39 +0200

    Start Project for Recruitment Task Atipera company

--- src/main/java/com/example/taskatipera/dto/GitRepositoryDTO.java
+++ src/main/java/com/example/taskatipera/dto/GitRepositoryDTO.java
@@ -2,5 +2,6 @@ package com.example.taskatipera.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
 public record GitRepositoryDTO(boolean forkies, @JsonProperty("full_name") String name, @JsonProperty ("branches_url")String url) {
 }
